a23d8cb2b039327a95d8bf16b0b7a6dc256ca7e8
dsk502 <shengkang2023@outlook.com>
2025-03-05 18:15:12 +0800

    Add RSAUtils class

---
+++ app/src/main/java/com/simon/homeirrigationclient/model/RSAUtils.java
@@ -0,0 +1,59 @@
+package com.simon.homeirrigationclient.model;
+
+import java.security.KeyFactory;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.NoSuchAlgorithmException;
+import java.security.PrivateKey;
+import java.security.PublicKey;
+import java.security.spec.PKCS8EncodedKeySpec;
+import java.security.spec.X509EncodedKeySpec;
+import java.util.Base64;
+
+import javax.crypto.Cipher;
+
+public class RSAUtils {
+    public static KeyPair generateKeyPair() throws NoSuchAlgorithmException {
+        KeyPairGenerator keyGen = KeyPairGenerator.getInstance("RSA");
+        keyGen.initialize(2048);
+        return keyGen.generateKeyPair();
+    }
+
+    //Encode the key to string
+    public static String encodeKey(byte[] keyBytes) {
+        return Base64.getEncoder().encodeToString(keyBytes);
+    }
+
+    //Load public key from key string
+    public static PublicKey loadPublicKey(String publicKeyStr) throws Exception {
+        byte[] keyBytes = Base64.getDecoder().decode(publicKeyStr);
+        X509EncodedKeySpec keySpec = new X509EncodedKeySpec(keyBytes);
+        KeyFactory keyFactory = KeyFactory.getInstance("RSA");
+        return keyFactory.generatePublic(keySpec);
+    }
+
+    //Load private key from key string
+    public static PrivateKey loadPrivateKey(String privateKeyStr) throws Exception {
+        byte[] keyBytes = Base64.getDecoder().decode(privateKeyStr);
+        PKCS8EncodedKeySpec keySpec = new PKCS8EncodedKeySpec(keyBytes);
+        KeyFactory keyFactory = KeyFactory.getInstance("RSA");
+        return keyFactory.generatePrivate(keySpec);
+    }
+
+    //Encrypt data
+    public static String encrypt(String data, PublicKey publicKey) throws Exception {
+        Cipher cipher = Cipher.getInstance("RSA/ECB/PKCS1Padding");
+        cipher.init(Cipher.ENCRYPT_MODE, publicKey);
+        byte[] encryptedBytes = cipher.doFinal(data.getBytes());
+        return Base64.getEncoder().encodeToString(encryptedBytes);
+    }
+
+    //Decrypt data
+    public static String decrypt(String encryptedData, PrivateKey privateKey) throws Exception {
+        byte[] encryptedBytes = Base64.getDecoder().decode(encryptedData);
+        Cipher cipher = Cipher.getInstance("RSA/ECB/PKCS1Padding");
+        cipher.init(Cipher.DECRYPT_MODE, privateKey);
+        byte[] decryptedBytes = cipher.doFinal(encryptedBytes);
+        return new String(decryptedBytes);
+    }
+}
